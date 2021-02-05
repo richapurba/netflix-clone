6217f6e8db0b317d112d9d604eb75319c9697aed
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-05 19:21:07 +0000

    Add home screen set up

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>netflix dude</h1>
+	  <HomeScreen />
     </div>
   );
 }
---
+++ netflix/src/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HomeScreen.css";
+
+function HomeScreen() {
+	return (
+		<div className="homeScreen">
+			{/* Navbar */}
+
+			{/* Banner */}
+
+			{/* Row */}
+		</div>
+	);
+}
+
+export default HomeScreen;
