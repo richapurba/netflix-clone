3d27257b6f935ebb2130da874787082da32c6fec
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-05 18:50:29 +0000

    Add firebase config

---
+++ netflix/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCJNDU35hyIvGL8d1MFXVjg8vFQlSYBz4",
+  authDomain: "netflix-clone-22f65.firebaseapp.com",
+  projectId: "netflix-clone-22f65",
+  storageBucket: "netflix-clone-22f65.appspot.com",
+  messagingSenderId: "1034707269720",
+  appId: "1:1034707269720:web:c369a91c6bbdcf36f7f22d"
+};
