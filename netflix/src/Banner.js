1adbd9a1474ec0ea0699b0ca5f62c02349d05c2a
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-06 06:46:51 +0000

    Create the basic of Netflix banner

---
+++ netflix/src/Banner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+	return (
+		<header className="banner" style={{
+			
+		}}>
+		
+		</header>
+	);
+}
+
+export default Banner;
