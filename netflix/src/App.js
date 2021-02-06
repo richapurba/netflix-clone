2fea656d6e501fa9eb5f8fa9aa2b7f946c30b578
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-06 17:38:26 +0000

    Add the basic of the login page

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
 	const user = null;
@@ -10,7 +11,7 @@ function App() {
     <div className="app">
 	  <Router>
 	  	{!user ? (
-			<Login />
+			<LoginScreen />
 		) : (
 			<Switch>
 				<Route exact path="/">
