e256e25842093a15c8ce7f1710787a3575ae946a
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-06 07:08:09 +0000

    Add the basic banner skeleton

--- netflix/src/Banner.js
+++ netflix/src/Banner.js
@@ -4,9 +4,19 @@ import "./Banner.css";
 function Banner() {
 	return (
 		<header className="banner" style={{
-			
+			backgroundImage: `url("https://mediaincanada.com/wp/wp-content/uploads/2020/09/netflix-banner.png")`,
+			backgroundSize: "cover",
+			backgroundPosition: "center center",
 		}}>
-		
+			<div className="banner__contents">
+				<h1 className="banner__title">Movie Name</h1>
+				<div className="banner__buttons">
+					<button className="banner__button">Play</button>
+					<button className="banner__button">My List</button>
+				</div>
+				<h1 className="banner__description">A testing description for the app banner</h1>
+			</div>
+			<div className="banner--fadeBottom" />
 		</header>
 	);
 }
