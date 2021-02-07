48712d15a5db27c7b7c09174889d66a228515459
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-07 04:35:15 +0000

    Style the plans screen

--- netflix/src/screens/PlansScreen.js
+++ netflix/src/screens/PlansScreen.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./PlansScreen.css";
 
 function PlansScreen() {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		db.collection("products").where("active", "==", true).get().then(querySnapshot => {
+			const products = {};
+			querySnapshot.forEach(async productDoc => {
+				products[productDoc.id] = productDoc.data();
+				const priceSnap = await productDoc.ref.collection("prices").get();
+				priceSnap.docs.forEach(doc => {
+					products[productDoc.id].prices = {
+						priceId: price.id,
+						priceData: price.data(),
+					};
+				});
+			});
+			setProducts(products);
+		});
+	}, []);
+
+	console.log(products);
+
+	const loadCheckout = async (priceId) => {
+
+	};
+
 	return (
 		<div className="plansScreen">
-		
+			{Object.entries(products).map(([productId, productData]) => {
+				return {
+					<div className="plansScreen__plan">
+						<div className="plansScreen__info">
+							<h5>{productData.name}</h5>
+							<h6>{productData.description}</h6>
+						</div>
+						<button onClick={() => loadCheckout(productData.prices.priceId)} >Subscribe</button>
+					</div>
+				};
+			})}
 		</div>
 	);
 }
