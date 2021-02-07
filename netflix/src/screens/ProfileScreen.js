a1ff774dcb413ff8d6f8acd2dbe98b01d1f2ac4d
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-07 03:56:39 +0000

    Add the base of plans screen

--- netflix/src/screens/ProfileScreen.js
+++ netflix/src/screens/ProfileScreen.js
@@ -4,6 +4,7 @@ import Nav from "../Nav";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
+import PlansScreen from "./PlansScreen";
 
 function ProfileScreen() {
 	const user = useSelector(selectUser);
@@ -18,6 +19,7 @@ function ProfileScreen() {
 						<h2>{user.email}</h2>
 						<div className="profileScreen__plans">
 							<h3>Plans</h3>
+							<PlansScreen />
 							<button onClick={() => auth.signOut()} className="profileScreen__signOut">Sign Out</button>
 						</div>
 					</div>
