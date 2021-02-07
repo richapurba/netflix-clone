700715fe443b3cfb643f340e3f999f48d26d089f
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-07 05:27:19 +0000

    Finish the whole app

--- netflix/src/screens/PlansScreen.js
+++ netflix/src/screens/PlansScreen.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from "react";
 import "./PlansScreen.css";
+import db from "../firebase";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { loadStripe } from "@stripe/stripe-js";
 
 function PlansScreen() {
 	const [products, setProducts] = useState([]);
+	const user = useSelector(selectUser);
+	const [subscription, setSubscription] = useState(null);
+
+	useEffect(() => {
+		db.collection('customers').doc(user.uid).collection("subscriptions").get().then(querySnapshot => {
+			querySnapshot.forEach(async subscription => {
+				setSubscription({
+					role: subscription.data().role,
+					current_period_end: subscription.data().current_period_end.seconds,
+					current_period_start: subscription.data().current_period_start.seconds,
+				});
+			});
+		});
+	}, [user.uid]);
 
 	useEffect(() => {
 		db.collection("products").where("active", "==", true).get().then(querySnapshot => {
@@ -24,19 +42,37 @@ function PlansScreen() {
 	console.log(products);
 
 	const loadCheckout = async (priceId) => {
-
+		const docRef = await db.collection("customers").doc(user.uid).collection("checkout_sessions").add({
+			price: priceId,
+			success_url: window.location.origin,
+			cancel_url: window.location.origin,
+		});
+		docRef.onSnapshot(async(snap) => {
+			const { error, sessionId } = snap.data();
+			if (error) {
+				alert(`An error occured: ${error.message}`);
+			}
+			if (sessionId) {
+				const stripe = await loadStripe("pk_test_51IBkYpJLYiXcTDTowhJ8rao2IvhWcYgfMR65s6ayFjUaKcy368jW5fgnIMkcDI9nj1r0uJXVeurucmLFx84ojRyG00m2jKiVuj");
+				stripe.redirectToCheckout({ sessionId });
+			}
+		})
 	};
 
 	return (
 		<div className="plansScreen">
+		<br />
+		{subscription && <p>Renewal date: {new Date(subscription?.current_period_end * 1000).toLocalDateString()}</p>}
 			{Object.entries(products).map(([productId, productData]) => {
+				const isCurrentPackage = productData.name?.toLowerCase().includes(subscription?.role);
+
 				return {
-					<div className="plansScreen__plan">
+					<div key={productId} className={`${isCurrentPackage && "plansScreen__plan--disabled"} plansScreen__plan`}>
 						<div className="plansScreen__info">
 							<h5>{productData.name}</h5>
 							<h6>{productData.description}</h6>
 						</div>
-						<button onClick={() => loadCheckout(productData.prices.priceId)} >Subscribe</button>
+						<button onClick={() => !isCurrentPackage && loadCheckout(productData.prices.priceId)} >{isCurrentPackage ? 'Current Package' : 'Subscribe'}</button>
 					</div>
 				};
 			})}
